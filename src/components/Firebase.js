455de52b98dfb995273608af01f9850d118db039
reobest <sabdelrahman071@gmail.com>
2024-02-04 10:29:51 -0800

    updated!

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,15 +1,7 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore';
 import {getAuth} from 'firebase/auth'
-  const firebaseConfig = process.env.FIRE_BASE_CONFIG || {
-    apiKey: "AIzaSyChgnTRilFfbLwO9sj07xf7xFFrBvwT5Z4",
-    authDomain: "fir-rayan-b57cf.firebaseapp.com",
-    projectId: "fir-rayan-b57cf",
-    storageBucket: "fir-rayan-b57cf.appspot.com",
-    messagingSenderId: "497358466829",
-    appId: "1:497358466829:web:3417f16e1020dab05f0a44",
-    measurementId: "G-1VCYPCPW2L"
-  }
+  const firebaseConfig = process.env.FIRE_BASE_CONFIG 
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
   const auth = getAuth(app)
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect} from 'react';
 import styled from 'styled-components';
 import List from './SideBarList';
 import {FaPen} from 'react-icons/fa';
